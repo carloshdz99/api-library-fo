111f3549a880c7e4cddeeb6dde8dc7e0f31c4d7e
carloshdz99 <carloshdzfunes@gmail.com>
2022-10-02 18:33:01 -0600

    :hammer: Correccion

--- config/server.js
+++ config/server.js
@@ -14,7 +14,7 @@ class Server {
     constructor() {
         this.app = express();
         this.server = createServer(this.app);
-        this.port = process.env.PORT || 3050;
+        this.port = process.env.PORT || 5000;
         this.host = process.env.HOST || 'localhost';
         this.middlewares();
     }
