0114dc289b64bd3a2d52faef616b458623d8b43e
carloshdz99 <carloshdzfunes@gmail.com>
2022-10-06 21:47:58 -0600

    :hammer: Correccion archivo DB.js

--- app/nucleo/DB.js
+++ app/nucleo/DB.js
@@ -6,7 +6,7 @@ const { DB_USERNAME, DB_HOST, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 
 class DB {
     static connection(connection = null) {
-        return new Sequelize('d9mmkq080bbodk', 'yyfftgmmrurqni', 'f9e907c556904e5394504696faff7b93f58cd7535a2d39f34aeb4057c2848413', {
+        return new Sequelize('d9mmkq080bbodk', 'yyfftgmmrurqni', '8a62fe1820d2d7980ce4e5ab97fc6151fb53203e7c26fb8a012b6f4382617cd3', {
             host: 'ec2-3-230-122-20.compute-1.amazonaws.com',
             port: 5432,
             dialect: 'postgres',
